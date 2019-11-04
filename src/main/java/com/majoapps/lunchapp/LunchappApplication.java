79d4f2806cf3efb61c1f32894a5dbe8a2be78f53
markcardamis <markncardamis@gmail.com>
2019-11-05 00:29:09 +1100

    init commit

---
+++ src/main/java/com/majoapps/lunchapp/LunchappApplication.java
@@ -0,0 +1,13 @@
+package com.majoapps.lunchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/majoapps/lunchapp/LunchappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majoapps.lunchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
