12e679519a089009dda94fa6da6bfbf3a4574a78
markcardamis <markncardamis@gmail.com>
2019-11-11 01:16:18 +1100

    Moved startup calls from Runner to LunchService

--- src/main/java/com/majoapps/lunchapp/MyRunner.java
+++ src/main/java/com/majoapps/lunchapp/MyRunner.java
@@ -2,8 +2,10 @@ package com.majoapps.lunchapp;
 
 import com.majoapps.lunchapp.business.domain.IngredientDto;
 import com.majoapps.lunchapp.business.domain.RecipeDto;
-import com.majoapps.lunchapp.business.service.IngredientClientService;
-import com.majoapps.lunchapp.business.service.RecipeClientService;
+import com.majoapps.lunchapp.business.service.IngredientService;
+import com.majoapps.lunchapp.business.service.RecipeService;
+import com.majoapps.lunchapp.business.service.RestTemplateService;
+
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-@Slf4j
 @Component
 public class MyRunner implements CommandLineRunner {
 
@@ -21,19 +22,8 @@ public class MyRunner implements CommandLineRunner {
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
     }
-    
-    @Autowired
-    private IngredientClientService ingredientClientService;
-
-    @Autowired
-    private RecipeClientService recipeClientService;
 
     @Override
     public void run(String... args) throws Exception {
-        // run Rest Template service on startup to fetch data on startup.
-        List<RecipeDto> recipes = recipeClientService.saveRecipes();
-        List<IngredientDto> ingredients = ingredientClientService.saveIngredients();
-        log.debug(recipes.toString());
-        log.debug(ingredients.toString());
     }
 }
