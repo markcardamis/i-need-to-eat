4088ba5b019a27e21c86c88253ff8e30d3d8bfde
markcardamis <markncardamis@gmail.com>
2019-11-10 15:45:53 +1100

    rearrange import to meet Google style

--- src/main/java/com/majoapps/lunchapp/LunchappApplication.java
+++ src/main/java/com/majoapps/lunchapp/LunchappApplication.java
@@ -1,7 +1,7 @@
 package com.majoapps.lunchapp;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class LunchappApplication {
--- src/main/java/com/majoapps/lunchapp/data/repository/RecipeRepository.java
+++ src/main/java/com/majoapps/lunchapp/data/repository/RecipeRepository.java
@@ -1,9 +1,9 @@
 package com.majoapps.lunchapp.data.repository;
 
 import com.majoapps.lunchapp.data.entity.Recipe;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface RecipeRepository extends CrudRepository <Recipe, Integer> {
