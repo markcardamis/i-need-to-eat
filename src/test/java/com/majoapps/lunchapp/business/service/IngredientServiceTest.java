f134311e706d18b64318d32acf8189f1a77b1cf6
markcardamis <markncardamis@gmail.com>
2019-11-15 02:05:11 +1100

    Added Mockito test for Ingredient and recipe service

--- src/test/java/com/majoapps/lunchapp/business/service/IngredientServiceTest.java
+++ src/test/java/com/majoapps/lunchapp/business/service/IngredientServiceTest.java
@@ -1,47 +1,87 @@
 package com.majoapps.lunchapp.business.service;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.majoapps.lunchapp.business.domain.IngredientDto;
+import com.majoapps.lunchapp.business.domain.IngredientDtoWrapper;
 import com.majoapps.lunchapp.data.entity.Ingredient;
 import com.majoapps.lunchapp.data.repository.IngredientRepository;
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest()
+@RunWith(MockitoJUnitRunner.class)
 @ActiveProfiles("test")
 class IngredientServiceTest {
-
-    @Autowired
-    private IngredientService ingredientService;
-
-    @Autowired
+    private final String INGREDIENT_JSON_FILENAME = "src/test/java/com/majoapps/lunchapp/ingredients.json";
+    private ObjectMapper objectMapper;
+    
+    @Mock
     private IngredientRepository ingredientRepository;
 
+    @InjectMocks
+    private IngredientService ingredientService;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.initMocks(this);
+        objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        objectMapper.registerModule(javaTimeModule);
+    }
+    
     @Test
-    void getIngredientsFromDatabase() throws Exception {
-        Iterable<Ingredient> ingredients = ingredientService.getIngredientRepository().findAll();
-        ingredients.forEach(ingredient -> {
-            System.out.println(ingredient);
-        });
-        assertNotNull(ingredients);
+    @DisplayName("Test save ingredient to repository")
+    void saveIngredient() throws Exception {
+        IngredientDtoWrapper ingredientDtoWrapper = objectMapper.readValue
+                (new File(INGREDIENT_JSON_FILENAME), IngredientDtoWrapper.class);
+        List<IngredientDto> ingredientDtos =  ingredientDtoWrapper.getIngredients();
+
+        when(ingredientRepository.save(any(Ingredient.class))).thenReturn(new Ingredient());
+
+        if (ingredientDtos.size() > 0) {  //only proceed if loaded sample local json file
+            Ingredient ingredientCreated = ingredientService.saveIngredient(ingredientDtos.get(0));
+            assertEquals(ingredientCreated.getTitle(), ingredientDtos.get(0).getTitle());
+        } else {
+            assertTrue(false);
+        }
     }
 
     @Test
-    void getIngredientsByOrderByBestBeforeAsc() throws Exception {
-        Iterable<Ingredient> ingredients = ingredientRepository.findAllByOrderByBestBeforeAsc();
-        ingredients.forEach(ingredient -> {
-            System.out.println(ingredient);
-        });
-        assertNotNull(ingredients);
+    void findByUseByAfter() throws Exception {
+
+        // set an ingredient with good use-by date
+        List<Ingredient> ingredientsArrayList = new ArrayList<>();
+        Ingredient ingredientGood = new Ingredient();
+        ingredientGood.setId(1);
+        ingredientGood.setTitle("Ham");
+        ingredientGood.setBestBefore(LocalDate.now().minusDays(2));
+        ingredientGood.setUseBy(LocalDate.now().plusDays(2));
+        ingredientsArrayList.add(ingredientGood);
+        
+        when(ingredientRepository.findByUseByAfter(any())).thenReturn(ingredientsArrayList);
+
+        List<Ingredient> ingredients = ingredientService.findByUseByAfter(LocalDate.now());
+        assertTrue(ingredients.size() == 1);
+        assertEquals(ingredients.get(0).getTitle(), ingredientGood.getTitle());
     }
 
-    @Test
-    void saveIngredient() {
-    }
-
-    @Test
-    void findByUseByAfter() {
-    }
 }
--- src/test/java/com/majoapps/lunchapp/business/service/RecipeServiceTest.java
+++ src/test/java/com/majoapps/lunchapp/business/service/RecipeServiceTest.java
@@ -1,51 +1,98 @@
 package com.majoapps.lunchapp.business.service;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.majoapps.lunchapp.business.domain.RecipeDto;
+import com.majoapps.lunchapp.business.domain.RecipeDtoWrapper;
 import com.majoapps.lunchapp.data.entity.Recipe;
 import com.majoapps.lunchapp.data.repository.RecipeRepository;
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // for restTemplate
+@RunWith(MockitoJUnitRunner.class)
 @ActiveProfiles("test")
 class RecipeServiceTest {
+    private final String RECIPE_JSON_FILENAME = "src/test/java/com/majoapps/lunchapp/recipes.json";
+    private ObjectMapper objectMapper;
 
-    @Autowired
-    private RecipeService recipeService;
-
-    @Autowired
+    @Mock
     private RecipeRepository recipeRepository;
 
-    @Test
-    void getRecipesFromDatabase() throws Exception {
-        Iterable<Recipe> recipes = recipeRepository.findAll();
-        recipes.forEach(recipe -> {
-            System.out.println(recipe);
-        });
-        assertNotNull(recipes);
+    @InjectMocks
+    private RecipeService recipeService;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.initMocks(this);
+        objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        objectMapper.registerModule(javaTimeModule);
     }
 
     @Test
-    void getRecipesFromDatabaseViaService() throws Exception {
-        Iterable<Recipe> recipes = recipeService.getRecipeRepository().findAll();
-        recipes.forEach(recipe -> {
-            System.out.println(recipe);
-        });
-        assertNotNull(recipes);
+    @DisplayName("Test save recipe to repository")
+    void saveRecipe() throws Exception {
+        RecipeDtoWrapper recipeDtoWrapper = objectMapper.readValue
+                (new File(RECIPE_JSON_FILENAME), RecipeDtoWrapper.class);
+        List<RecipeDto> recipeDtos =  recipeDtoWrapper.getRecipes();
+
+        when(recipeRepository.save(any(Recipe.class))).thenReturn(new Recipe());
+
+        if (recipeDtos.size() > 0) {  //only proceed if loaded sample local json file
+            Recipe recipeCreated = recipeService.saveRecipe(recipeDtos.get(0));
+            assertEquals(recipeCreated.getTitle(), recipeDtos.get(0).getTitle());
+        } else {
+            assertTrue(false);
+        }
     }
 
     @Test
-    void saveRecipe() {
+    void findByTitle() throws Exception {
+        List<Recipe> recipeArrayList = new ArrayList<>();
+        Recipe recipe = new Recipe();
+        recipe.setId(1);
+        recipe.setTitle("HotDog");
+        recipeArrayList.add(recipe);
+        
+        when(recipeRepository.findByTitle(any())).thenReturn(recipeArrayList);
+
+        List<Recipe> recipes = recipeService.findByTitle("HotDog");
+        assertTrue(recipes.size() == 1);
+        assertEquals(recipes.get(0).getTitle(), recipe.getTitle());
     }
 
     @Test
-    void findByTitle() {
-    }
+    void findByIngredient() throws Exception {
+        List<Recipe> recipeArrayList = new ArrayList<>();
+        Recipe recipe = new Recipe();
+        recipe.setId(1);
+        recipe.setTitle("HotDog");
+        recipe.setIngredient("Bread");
+        recipeArrayList.add(recipe);
+        
+        when(recipeRepository.findByIngredient(any())).thenReturn(recipeArrayList);
 
-    @Test
-    void findByIngredient() {
+        List<Recipe> recipes = recipeService.findByIngredient("Bread");
+        assertTrue(recipes.size() == 1);
+        assertEquals(recipes.get(0).getIngredient(), recipe.getIngredient());
     }
 }
